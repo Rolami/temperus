d47f2747c7b0c0012ec8dca94df5288e5b59789f
Robin <24991308+Rolami@users.noreply.github.com>
2024-04-17 08:58:57 +0200

    Commit to remake the app structure +some progress.

--- temperus/Program.cs
+++ temperus/Program.cs
@@ -6,6 +6,7 @@ var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddRazorComponents()
+    .AddInteractiveServerComponents()
     .AddInteractiveWebAssemblyComponents();
 
 builder.Services.AddMudServices();
@@ -30,6 +31,7 @@ app.UseStaticFiles();
 app.UseAntiforgery();
 
 app.MapRazorComponents<App>()
+    .AddInteractiveServerRenderMode()
     .AddInteractiveWebAssemblyRenderMode()
     .AddAdditionalAssemblies(typeof(Counter).Assembly);
 
